a36b69790d8e94120392c43bda992ead383f1267
KodiKraig <cody.craig@aol.com>
2023-06-24 13:22:27 -0400

    Update config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      // Rewrite everything else to use `pages/index`
-      // This allows us to have an SPA and not use NextJS SSR for every page
-      // However, NextJS will priortize the rewrites meaning, if a /page exists then we can load it and continue to use SSR!
-      {
-        source: '/:any*',
-        destination: '/'
-      }
-    ];
-  },
   reactStrictMode: true,
   swcMinify: true
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,16 @@ import { Logo, Text } from '@components/ui';
 const Home = () => {
   return (
     <Container>
-      <div className="flex flex-col items-center">
-        <div className="relative w-screen h-96">
-          <Logo />
-        </div>
+      <div className="relative w-screen h-96">
+        <Logo />
+      </div>
 
-        <div className="flex flex-col items-center py-16">
-          <Text variant="sectionHeading">Next JS Starter Project</Text>
-          <Text>
-            This is a starter project with all the basic dependencies and
-            project structure to quickly get started on a new NextJS 12 project.
-          </Text>
-        </div>
+      <div className="p-8">
+        <Text variant="sectionHeading">Next JS Starter Project</Text>
+        <Text>
+          This is a starter project with all the basic dependencies and project
+          structure to quickly get started on a new NextJS 12 project.
+        </Text>
       </div>
     </Container>
   );
